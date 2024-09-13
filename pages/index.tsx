814d1d121076bacf432edc6e3bd2b74470ed4682
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2024-09-13 19:55:47 +0530

    Refactor and add new SVG icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const gilroyRegular = localFont({
+  src: "./fonts/Gilroy/Gilroy-Regular.ttf",
+  display: "swap"
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const gilroyBold = localFont({
+  src: "./fonts/Gilroy/Gilroy-Bold.ttf",
+  display: "swap"
 });
+
 
 export default function Home() {
   return (
-    <div>
-      Hello World!
+    <div className="">
+      <div className="logo">
+        <Image src="/logos/logo.svg" alt="Logo" width={100} height={100} />
+      </div>
+      <div className={gilroyBold.className}>
+        Hello World!
+      </div>
     </div>
   );
 }
