3823792872c1dff9e1c685644beda92179e31248
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2024-09-14 00:30:05 +0530

    Refactor TopFilter component and fix search functionality

--- components/TopFilter/index.tsx
+++ components/TopFilter/index.tsx
@@ -4,13 +4,13 @@ const index = () => {
   return (
     <div className='px-10 py-8 w-full flex items-start justify-center flex-col'>
       <div className='flex items-center justify-start font-gilroyBold text-grayText'>
-        About {`x`} Trademarks found for "{`y`}"
+        About {`x`} Trademarks found for &quot;{`y`}&quot;
       </div>
       <hr className='w-full border-[1px] my-6' />
       <div className='flex items-center justify-start gap-4 font-gilroyBold text-grayText'>
         Also try searching for 
         <div className='bg-tertiary px-6 py-1 border border-secondary text-secondary rounded-xl'>
-          {` y`}
+          {`y`}
         </div>
       </div>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import localFont from "next/font/local";
-
-// Import fonts
-const gilroyRegular = localFont({
-  src: "./fonts/Gilroy/Gilroy-Regular.ttf",
-  display: "swap",
-  variable: "--gilroy-regular"
-});
-
-const gilroyBold = localFont({
-  src: "./fonts/Gilroy/Gilroy-Bold.ttf",
-  display: "swap",
-  variable: "--gilroy-bold"
-});
-
-const gilroySemibold = localFont({
-  src: "./fonts/Gilroy/Gilroy-Semibold.ttf",
-  display: "swap",
-  variable: "--gilroy-semibold"
-});
+// import localFont from "next/font/local";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
