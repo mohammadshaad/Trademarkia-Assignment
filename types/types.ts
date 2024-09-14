91ada85673065dfdfec464d0ff81e684ac14b6c3
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2024-09-14 17:11:58 +0530

    Refactor types/types.ts to add ApiResponseItem interface

--- types/types.ts
+++ types/types.ts
@@ -1,7 +1,18 @@
+
 export interface SearchResult {
-    name: string;
-    company: string;
-    markId: string;
-    date: string;
-    class: string;
-  }
+  name: string;
+  company: string;
+  markId: string;
+  date: string;
+  class: string;
+}
+
+export interface ApiResponseItem {
+  _source: {
+    mark_identification: string;
+    current_owner: string;
+    registration_number: string;
+    registration_date: number;
+    class_codes: string[];
+  };
+}
