3d0089d56075227b362e9fc3139cffcb4f8b2337
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2024-09-14 16:31:05 +0530

    Fetch the data from API through search

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,10 +1,49 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '@/public/logos/logo.svg';
 import search from '@/public/icons/search.svg';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [query, setQuery] = useState(''); 
+
+  const handleSearch = async () => {
+
+    const data = {
+      input_query: query,
+      input_query_type: "",
+      sort_by: "default",
+      status: [],
+      exact_match: false,
+      date_query: false,
+      owners: [],
+      attorneys: [],
+      law_firms: [],
+      mark_description_description: [],
+      classes: [],
+      page: 1,
+      rows: 10,
+      sort_order: "desc",
+      states: [],
+      counties: []
+    };
+
+    try {
+      const response = await fetch('https://vit-tm-task.api.trademarkia.app/api/v3/us', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      console.log('Search Results:', result);
+    } catch (error) {
+      console.error('Error during search:', error);
+    }
+  };
+
   return (
     <div className='bg-[#F8FAFE] border-b-4 border-[#EAF1FF] w-full flex items-center justify-start gap-6'>
       <div className='py-8 px-14'>
@@ -20,9 +59,14 @@ const Navbar = () => {
             type="text"
             placeholder='Search Trademark Here eg. Mickey Mouse '
             className="w-full bg-transparent outline-none text-lg ml-2 gilroy-regular placeholder:text-[#636363] placeholder:text-sm font-medium"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)} 
           />
         </div>
-        <button className="font-gilroySemibold bg-primary hover:bg-blue-900 transition-all duration-200 font-bold text-sm text-white rounded-xl px-10 py-4 ml-2 gilroy-bold">
+        <button
+          onClick={handleSearch} 
+          className="font-gilroySemibold bg-primary hover:bg-blue-900 transition-all duration-200 font-bold text-sm text-white rounded-xl px-10 py-4 ml-2 gilroy-bold"
+        >
           Search
         </button>
       </div>
